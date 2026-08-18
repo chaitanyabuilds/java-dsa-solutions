c9cf646a756e4cd6de42330c29994fb24efc7256
Chaitanya Bargaje <chetanbargaje2006@gmail.com>
2026-08-18 12:34:10 +0530

    Add files via upload

---
+++ Patterns/CharacterPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CharacterPattern {
+    public static void main(String args[]){
+
+        int n = 5;
+        char ch = 'A';
+
+        for(int line = 1; line <=n; line++){
+            for(int chars = 1; chars<=line; chars++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
