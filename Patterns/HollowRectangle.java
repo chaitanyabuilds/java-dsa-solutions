d80486501a49af8d1fbc90b11626e3d34aa34b17
Chaitanya Bargaje <chetanbargaje2006@gmail.com>
2026-08-29 20:35:14 +0530

    Add files via upload

---
+++ Patterns/HollowRectangle.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class HollowRectangle {
+
+     public static void hollow_rectangle(int totRows, int totCols) {
+
+        // Outer loop
+        for (int i = 1; i <= totRows; i++) {
+
+            // Inner loop
+            for (int j = 1; j <= totCols; j++) {
+
+                // Cell - (i, j)
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+
+                    // Boundary cells
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        hollow_rectangle(4, 5);
+    }
+}
