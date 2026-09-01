073f9580bbe86bf1662f301efe7d172a1e374142
Chaitanya Bargaje <chetanbargaje2006@gmail.com>
2026-09-01 13:41:15 +0530

    Add files via upload

---
+++ Arrays/LargestNum.java
@@ -0,0 +1,23 @@
+public class LargestNum {
+
+    public static int largestNumber(int[] numbers) {
+
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+
+        return largest;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 6, 3, 5};
+
+        System.out.println("Largest number is: " + largestNumber(numbers));
+    }
+}
---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,28 @@
+
+public class LinearSearch{
+     public static int linearSearch(int[] numbers, int key) {
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] == key) {
+                return i; // Return index if found
+            }
+        }
+
+        return -1; // Return -1 if not found
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 12;
+
+        int index = linearSearch(numbers, key);
+
+        if (index == -1) {
+            System.out.println("NOT found");
+        } else {
+            System.out.println("Key is at index: " + index);
+        }
+    }
+}
